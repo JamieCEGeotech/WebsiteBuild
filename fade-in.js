5bf957c99b31313c00c6eaec6bfffa2b674c45b8
Jamie <jamienoble.work@gmail.com>
2025-09-11 09:23:43 +0100

    + fade effect, sitemap, slideshow banner, maps

---
+++ fade-in.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const faders = document.querySelectorAll('.fade-in');
+    const appearOptions = { threshold: 0.15 };
+    const appearOnScroll = new IntersectionObserver(function(entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
